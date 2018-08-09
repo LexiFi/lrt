00de6014f3ebe88300ba0550331346f158827431
Patrik Keller <patrik@keller-re.de>
2018-08-09 11:03:33 +0200

    use inline tests

--- test/one.ml
+++ test/one.ml
@@ -4,5 +4,10 @@ type t = int * int
 
 let t : t ttype = DT_tuple [ DT_int ; DT_int ] |> Obj.magic
 
-let () =
-  print_stype Format.std_formatter (stype_of_ttype t)
+let stdout = Format.std_formatter
+let flush fmt = Format.pp_print_flush fmt ()
+
+let%expect_test _ =
+  print_stype stdout (stype_of_ttype t) ;
+  flush stdout ;
+  [%expect{| (int * int) |}]
