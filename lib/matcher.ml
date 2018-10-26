6866899593a03b372cdf0ea487b4be8cb70cae5a
Patrik Keller <patrik@keller-re.de>
2018-10-26 17:02:09 +0200

    Remove solved todo

--- lib/matcher.ml
+++ lib/matcher.ml
@@ -163,7 +163,6 @@ end
 module Make (Return : sig
   type 'a t
 end) : S with type 'a return = 'a Return.t = struct
-  (* TODO: document, that symbol table is stored in Module, not in Matcher.t *)
 
   module Index = Matcher_index.Tree
   module IntMap = Ext.Int.Map
